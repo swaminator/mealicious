86ffd79b7c8f3257b6aa360b8c4825f30b50844d
Swaminathan <nsswamin@3c22fb7b6676.ant.amazon.com>
2020-07-01 23:24:43 -0700

    merged stash changed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header'
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+Amplify.configure(awsExports);
+
 
 ReactDOM.render(
   <Router>
